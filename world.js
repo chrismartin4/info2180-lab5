e09db6365a7b260aa69866940d8022adf3f6bc0b
Christopher Martin <chris.b411@gmail.com>
2020-11-17 20:05:49 -0500

    Completed Exercise 5

--- world.js
+++ world.js
@@ -1,9 +1,30 @@
 window.onload = function () {
     var lookupbutton = document.getElementById("lookup");
+    var lookupcity = document.getElementById("cities");
     lookupbutton.addEventListener('click', function (i) {
         i.preventDefault();
         var lookupquery = document.getElementById("country").value;
         var url = "world.php?country=" + lookupquery;
+        var httprequest = new XMLHttpRequest();
+        httprequest.onreadystatechange = function () {
+            if (httprequest.readyState == XMLHttpRequest.DONE) {
+                if (httprequest.status == 200) {
+                    var country = httprequest.responseText;
+                    var result = document.getElementById("result");
+                    result.innerHTML = country;
+                } else {
+                    alert("Error");
+                }
+            }
+        }
+        httprequest.open("GET", url, true);
+        httprequest.send();
+    });
+
+    lookupcity.addEventListener('click', function(i){
+        i.preventDefault();
+        var lookupquery = document.getElementById("country").value;
+        var url = "world.php?country=" + lookupquery + "&context=cities";
         var httprequest = new XMLHttpRequest();
         httprequest.onreadystatechange = function () {
             if (httprequest.readyState == XMLHttpRequest.DONE) {
--- world.php
+++ world.php
@@ -7,25 +7,51 @@ $dbname = 'world';
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
 $country = filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
-$locate = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-$results = $locate->fetchAll(PDO::FETCH_ASSOC);
+$context = filter_input(INPUT_GET, "context", FILTER_SANITIZE_STRING);
+
+if ($context == "cities") {
+	$locate = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities INNER JOIN countries ON cities.country_code = countries.code WHERE countries.name LIKE '%$country%'");
+}else {
+	$locate = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+}
+
+$results= $locate->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
-<table>
-  <tr>
-    <th>Name</th>
-    <th>Continent</th>
-    <th>Independence Year</th>
-    <th>Head of State</th>
-  </tr>
-
-  <?php foreach ($results as $row): ?>
+<?php if($context == "cities"): ?>
+  <table>
     <tr>
-      <td><?php echo $row['name']; ?></td>
-      <td><?php echo $row['continent']; ?></td>
-      <td><?php echo $row['independence_year']; ?></td>
-      <td><?php echo $row['head_of_state']; ?></td>
+      <th>Name</th>
+      <th>District</th>
+      <th>Population</th>
     </tr>
-  <?php endforeach; ?>
-</table>
+
+    <?php foreach ($results as $row): ?>
+      <tr>
+        <td><?php echo $row['name']; ?></td>
+        <td><?php echo $row['district']; ?></td>
+        <td><?php echo $row['population']; ?></td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+
+<?php else: ?>
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>Continent</th>
+      <th>Independence Year</th>
+      <th>Head of State</th>
+    </tr>
+
+    <?php foreach ($results as $row): ?>
+      <tr>
+        <td><?php echo $row['name']; ?></td>
+        <td><?php echo $row['continent']; ?></td>
+        <td><?php echo $row['independence_year']; ?></td>
+        <td><?php echo $row['head_of_state']; ?></td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+<?php endif ?>
